ee2b237592f08ec97f24b05b96cb85d4a782478f
Md. Sazzad Siddique Likhon <62556794+MDSAZZADSIDDIQUE@users.noreply.github.com>
2023-03-23 23:39:30 +0600

    Add files via upload

---
+++ Reading characters until a blank is read.asm
@@ -0,0 +1,17 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+.CODE
+MAIN PROC FAR
+    MOV AX, @DATA
+    MOV DS, AX
+    MOV AH, 1
+    REPEAT:
+    INT 21H
+    CMP AL, ' '
+    JNE REPEAT
+    EXIT:
+    MOV AH, 4CH
+    INT 21H
+    MAIN ENDP
+END MAIN
